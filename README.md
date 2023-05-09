46c24ab85d0ef6aafcf86451fd443270f7757676
rishavsinghrnfi <82799738+rishavsinghrnfi@users.noreply.github.com>
2023-05-09 16:06:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# QR-base64-upload
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/imagetobase64-dzswun)
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  name = 'Angular';
+
+   private imageSrc: string = '';
+
+  handleInputChange(e) {
+    var file = e.dataTransfer ? e.dataTransfer.files[0] : e.target.files[0];
+    var pattern = /image-*/;
+    var reader = new FileReader();
+    if (!file.type.match(pattern)) {
+      alert('invalid format');
+      return;
+    }
+    reader.onload = this._handleReaderLoaded.bind(this);
+    reader.readAsDataURL(file);
+  }
+  _handleReaderLoaded(e) {
+    let reader = e.target;
+    this.imageSrc = reader.result;
+    console.log(this.imageSrc)
+  }
+}
